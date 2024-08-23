e1348366dba26d183e35cfab94a3027e33779ce0
George Vagdas <dev@vagdas.eu>
2024-08-23 13:08:09 +0300

    Added metrics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
-
 import netlify from "@astrojs/netlify";
+
+import webVitals from "@astrojs/web-vitals";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), db()],
+  integrations: [react(), tailwind(), db(), webVitals()],
   output: 'server',
   adapter: netlify()
 });
